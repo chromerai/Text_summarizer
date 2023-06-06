0ce037e9beb9b552dc054289f199c08805043f61
chromerai <kaustubhmishra983@gmail.com>
2023-06-05 22:08:38 -0400

    logging and exception handling

---
+++ main.py
@@ -0,0 +1,3 @@
+from Project_text_summarizer.logging import logger
+
+logger.info('welcome to custom logging')
--- setup.py
+++ setup.py
@@ -24,6 +24,6 @@ setuptools.setup(
     project_urls = {
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
-    package_dir = {"":"src"},
+    package_dir = {"": "src"},
     packages = setuptools.find_packages(where= "src")
 )
